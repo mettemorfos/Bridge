75c52feaf432966bf83ba6b6fdb4b16324ba8dd7
Mette Broegaard <mette.broegaard@Mette-Broegaard.local>
2025-12-19 09:47:39 +0100

    Add project

---
+++ Bridge/Bridge/BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeApp.swift
+//  Bridge
+//
+//  Created by Mette Broegaard on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bridge/Bridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bridge
+//
+//  Created by Mette Broegaard on 2025-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
