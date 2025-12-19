39aa8d51ca374e6480feb39f07b3ccbcc3cb4f79
Mette Broegaard <mette.broegaard@Mette-Broegaard.local>
2025-12-19 10:36:02 +0100

    Add login components

--- Bridge/Bridge/LoginView.swift
+++ Bridge/Bridge/LoginView.swift
@@ -12,7 +12,16 @@ struct LoginView: View {
     @EnvironmentObject var sessionManager: SessionManager
     
     var body: some View {
-        Text("Log in")
+        Text("Enter your credentials here:")
+        VStack(spacing: 15) {
+            TextField("Email", text: .constant(""))
+                .keyboardType(.emailAddress)
+                .textContentType(.emailAddress)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+            SecureField("Password", text: .constant(""))
+        }
+        .padding(20)
         Button("Log in") {
             sessionManager.login()
         }
@@ -21,4 +30,5 @@ struct LoginView: View {
 
 #Preview {
     LoginView()
+        .environmentObject(SessionManager())
 }
