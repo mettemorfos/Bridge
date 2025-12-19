4d6319b462ee3078f3ae203f3d17cc7fecc977fe
Mette Broegaard <mette.broegaard@Mette-Broegaard.local>
2025-12-19 11:50:02 +0100

    Add test webview

--- Bridge/Bridge/AuthView.swift
+++ Bridge/Bridge/AuthView.swift
@@ -6,15 +6,56 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct AuthView: View {
     @EnvironmentObject var sessionManager: SessionManager
+    @State private var page: WebPage?
+    private let datastore: WKWebsiteDataStore = .nonPersistent()
     var body: some View {
-        Text("Authenticate")
-        Button("Submit") {
-            sessionManager.authenticate()
+        
+        Group {
+            if let page {
+                WebView(page)
+            } else {
+                Text("Waiting")
+            }
         }
+        .onAppear() {
+            initWebpage()
+            loadContents()
+        }
+        
+        
     }
+    
+    func initWebpage() {
+            var configuration = WebPage.Configuration()
+            
+            var navigationPreference = WebPage.NavigationPreferences()
+            
+            navigationPreference.allowsContentJavaScript = true
+            navigationPreference.preferredHTTPSNavigationPolicy = .errorOnFailure
+            navigationPreference.preferredContentMode = .recommended
+            configuration.defaultNavigationPreferences = navigationPreference
+            
+            configuration.websiteDataStore = self.datastore
+            
+            configuration.applicationNameForUserAgent = "AuthView"
+            
+            let page = WebPage(configuration: configuration)
+            
+            self.page = page
+            
+        }
+        
+        func loadContents() {
+            guard let url = URL(string: "https://svt.se") else {
+                return
+            }
+            page?.load(URLRequest(url: url))
+        }
+
 }
 
 #Preview {
